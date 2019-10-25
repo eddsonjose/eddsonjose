59af9f8a43c4f2ecebdbbb0c32ee2f814d97d653
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-10-24 23:19:35 -0700

    update

---
+++ sketch.js
@@ -0,0 +1,13 @@
+//Official Github Website
+//by Eddson Jose
+
+//------------------------------------------------------------------
+function setup() {
+  // noCanvas();
+  createCanvas(windowWidth, windowHeight);
+}
+//------------------------------------------------------------------
+function draw() {
+  fill(255);
+  circle(width/2, height/2, 40);
+}
